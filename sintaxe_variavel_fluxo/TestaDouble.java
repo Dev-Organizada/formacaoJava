4a40b3789426570165b21a0806c9499c4b7cedc0
Nathaly Rodrigues <100220606+Dev-Organizada@users.noreply.github.com>
2022-10-17 10:33:58 -0300

    Sintaxe de variavel

---
+++ sintaxe_variavel_fluxo/TestaDouble.java
@@ -0,0 +1,12 @@
+package sintaxe_variavel_fluxo;
+
+public class TestaDouble {
+	public static void main(String[] args) {
+		double salario = 6800.50;
+		double diaria = salario / 30;
+		String frase = "Sua diaria é ";
+
+		System.out.println(frase + diaria);
+	}
+
+}
