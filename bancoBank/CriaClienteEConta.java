fe5b55acf7e39ce16ec9bea3f743cfdc809043c5
Nathaly Rodrigues <100220606+Dev-Organizada@users.noreply.github.com>
2022-10-17 12:01:01 -0300

    Adaptação dos atributos para get e set

--- bancoBank/CriaClienteEConta.java
+++ bancoBank/CriaClienteEConta.java
@@ -5,30 +5,32 @@ public class CriaClienteEConta {
 	public static void main(String[] args) {
 		Cliente primeiroCliente = new Cliente();
 
-		primeiroCliente.nome = "Nathaly Rodrigues";
-		primeiroCliente.cpf = "012.345.678-90";
-		primeiroCliente.profissao = "Analista de Desenvolvimento Oracle";
+		primeiroCliente.setNome("Nathaly Rodrigues");
+		primeiroCliente.setCpf("012.345.678-90");
+		primeiroCliente.setProfissao("Analista de Desenvolvimento Oracle");
 
 		Cliente segundoCliente = new Cliente();
 
-		segundoCliente.nome = "Igor Souza";
-		segundoCliente.cpf = "210.543.876-09";
-		segundoCliente.profissao = "Analista de Teste";
+		segundoCliente.setNome("Igor Souza");
+		segundoCliente.setCpf("210.543.876-09");
+		segundoCliente.setProfissao("Analista de Teste");
 
 		Conta conta1 = new Conta();
 		Conta conta2 = new Conta();
 
+		conta1.setTitular(primeiroCliente);
 		conta1.deposita(1000);
-		conta1.saca(10);
-		conta1.verificaSaldo();
-		conta1.titular = primeiroCliente;
-
-		conta2.titular = segundoCliente;
+		//conta1.saca(10);
+		//conta1.getSaldo();
+		
+		
+		conta2.setTitular(segundoCliente);
+		conta2.deposita(50);
 
 		conta1.transfere(350, conta2);
-
-		System.out.println(conta1.titular.nome);
-		System.out.println(conta2.titular.nome);
+		
+		System.out.println(conta1.getTitular().getNome());
+		System.out.println(conta2.getTitular().getNome());
 
 	}
 
