b76d24e4293ef6eb012d1be16e9113b9aabe01c4
Nathaly Rodrigues <100220606+Dev-Organizada@users.noreply.github.com>
2022-10-17 10:34:43 -0300

    Sintaxe de repetição

---
+++ sintaxe_variavel_fluxo/TestaRepeticao.java
@@ -0,0 +1,14 @@
+package sintaxe_variavel_fluxo;
+
+public class TestaRepeticao {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 10; i++) {
+			if(i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
